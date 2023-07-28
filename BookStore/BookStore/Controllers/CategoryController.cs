0eec87ac13032a99eb33241769fc083692d5bc06
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-07-28 18:35:42 +0200

    Added buttons for category editing and deletion.

--- BookStore/BookStore/Controllers/CategoryController.cs
+++ BookStore/BookStore/Controllers/CategoryController.cs
@@ -35,5 +35,32 @@ namespace BookStore.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
