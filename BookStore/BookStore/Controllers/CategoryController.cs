9094c5c99d0f8dd922777814086d9d9a37eb51e1
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-07-28 11:16:00 +0200

    Category View now shows a table of Categories.

--- BookStore/BookStore/Controllers/CategoryController.cs
+++ BookStore/BookStore/Controllers/CategoryController.cs
@@ -15,7 +15,7 @@ namespace BookStore.Controllers
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
-            return View();
+            return View(objCategoryList);
         }
     }
 }
