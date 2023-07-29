5a7d0bb699841983bdbe63a7af1a2aab4f2299bd
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-07-29 18:50:38 +0200

    Added a repository interface and an implementation using generic types.

---
+++ BookStore/BookStore.DataAccess/Repository/Repository.cs
@@ -0,0 +1,54 @@
+﻿using BookStore.DataAccess.Data;
+using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class  
+    {
+        private ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        { 
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+
+    }
+}
