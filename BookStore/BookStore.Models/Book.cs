2720bee8433a6b126dcc52bed3c9b4b889a2cc9a
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-07-30 12:03:43 +0200

    Added foreign key for Category in Book entity class, also added ImageUrl.

--- BookStore/BookStore.Models/Book.cs
+++ BookStore/BookStore.Models/Book.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookStore.Models
 {
@@ -20,20 +21,25 @@ namespace BookStore.Models
         [Required]
         public string ISBN { get; set; }
         [Required]
-        [Range(1, 1000)]
+        [Range(1, 1000, ErrorMessage = "List Price must be betwen 1-1000")]
         [Display(Name = "List Price")]
         public double ListPrice { get; set; }
         [Required]
-        [Range(1, 1000)]
+        [Range(1, 1000, ErrorMessage = "Price for 1-49 must be betwen 1-1000")]
         [Display(Name = "Price for 1-49")]
         public double Price { get; set; }
         [Required]
-        [Range(1, 1000)]
+        [Range(1, 1000, ErrorMessage = "Price for 50-99 must be betwen 1-1000")]
         [Display(Name = "Price for 50-99")]
         public double Price50 { get; set; }
         [Required]
-        [Range(1, 1000)]
+        [Range(1, 1000, ErrorMessage = "Price for 100+ must be betwen 1-1000")]
         [Display(Name = "Price for 100+")]
         public double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
