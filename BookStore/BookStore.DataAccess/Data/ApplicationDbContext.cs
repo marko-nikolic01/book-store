36705cf56b68f236cc80449c67a624c5c83f336c
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-07-31 10:23:57 +0200

    Added  the Identity area.

--- BookStore/BookStore.DataAccess/Data/ApplicationDbContext.cs
+++ BookStore/BookStore.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using BookStore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
 using static System.Reflection.Metadata.BlobBuilder;
 
 namespace BookStore.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -16,6 +18,8 @@ namespace BookStore.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
